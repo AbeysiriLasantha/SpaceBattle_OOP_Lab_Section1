6e2440968c4b658847dda17c07c735f17e039f16
AbeysiriLasantha <lasantha.abeysisri@gmail.com>
2024-08-16 00:35:53 -0500

    Creating EarthShip object and 6 AlianShip objects

--- app.js
+++ app.js
@@ -18,3 +18,28 @@ class Ship {
     }
 }
 
+// Creating the EarthShip object 
+const earthShip = new Ship (20, 5, .7);
+    console.log (earthShip);
+
+// Creating the 6 AlianShip objects 
+// Define an array to hold the AlianShips 
+const alianShipArray = [];
+// Define the no. of AlianShips
+const noOfAlianShips = 6;
+
+for (let ships=1; ships<=noOfAlianShips; ships++) {
+    const hull = Math.floor(Math.random() * 4) + 3; // Range between 3 and 6
+    const firepower = Math.floor(Math.random() * 3) + 2; // Range between 2 and 4
+    const accuracy = Math.random() * 0.2 + 0.6; // Range between 0.6 and 0.8
+
+    const alianShip = new Ship (hull, firepower, accuracy);
+    alianShipArray.push(alianShip);
+}
+
+console.log (alianShipArray.length)
+
+// AlianShip Details 
+alianShipArray.forEach((newShip) => {
+    console.log (newShip);
+})
